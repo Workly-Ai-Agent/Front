211d97c5eddc84b3aeea5f45fc2202847fd6aa36
HP657 <junlee5070@gmail.com>
2026-09-20 01:52:45 +0900

    chore: backend url 주소 및 env 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
-  ],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    server: {
+      proxy: {
+        "/api": env.VITE_BACKEND_URL ?? "http://localhost:8080",
+      },
+    },
+    plugins: [
+      react(),
+      babel({ presets: [reactCompilerPreset()] }),
+      tailwindcss(),
+    ],
+  };
+});
